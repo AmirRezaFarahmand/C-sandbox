e96be1a1173e30aceb133a7b392feb24113cd9ca
AmirRezaFarahmand <96933338+AmirRezaFarahmand@users.noreply.github.com>
2024-12-09 22:24:31 +0330

    Create OnFib.c

---
+++ OnFib.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+#define MAXn (10010)
+
+int f[MAXn];
+
+int fib(int n)
+{
+    if (n == 1 || n == 0)
+        return 1;
+    
+    if (f[n])
+        return f[n];
+        
+    return f[n] = fib(n - 1) + fib(n - 2);;  
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    printf("%d", fib(n));
+}
